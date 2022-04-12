6677192f9ac51d85990adf0d000b9b4314eb092d
yjchoi <yjchoi@unisemiot.com>
2022-04-12 14:23:12 +0900

    2022-04-12 - jpa

--- src/main/java/com/example/new_test/controller/MainRestController.java
+++ src/main/java/com/example/new_test/controller/MainRestController.java
@@ -1,8 +1,12 @@
 package com.example.new_test.controller;
 
+import com.example.new_test.entity.DataTablesOutput;
 import com.example.new_test.entity.Member;
 import com.example.new_test.repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,10 +18,18 @@ public class MainRestController {
     private MemberRepository memberRepository;
 
     @GetMapping("/search")
-    public List<Member> search(String search) {
+    public List<Member> search(String search, int start, int length, int draw) {
+        int page = start / length;
+        Pageable pageable = PageRequest.of(page, length);
         List<Member> memberList;
+        Page<Member> responseData = memberRepository.findAll(pageable);
+
+        DataTablesOutput dataTablesOutput = new DataTablesOutput();
+
+        dataTablesOutput
+
         if (search.equals("") || search.equals("all")) {
-            memberList = memberRepository.findAll();
+            memberList = memberRepository.findAll(pageable);
         } else {
             memberList = memberRepository.findByNameContains(search);
         }
