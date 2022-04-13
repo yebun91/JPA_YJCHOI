8325c8859a32ef9f06ece851957a3841ef9745ea
yjchoi <yjchoi@unisemiot.com>
2022-04-13 17:30:25 +0900

    2022-04-13 - jpa 시작

--- src/main/java/com/example/new_test/controller/MainRestController.java
+++ src/main/java/com/example/new_test/controller/MainRestController.java
@@ -23,7 +23,22 @@ public class MainRestController {
         int draw = requestBody.getDraw();
         int length = requestBody.getLength();
         int start = requestBody.getStart();
-        String search = requestBody.getData().get("name");
+
+        // 검색하고 싶은 검색어
+        String search = requestBody.getSearch().get(DataTablesInput.SearchCriterias.value);
+
+        // 컬럼 이름을 가져오기 위해 몆 번째 컬럼인지 숫자를 가져옴
+        int culumnNum = Integer.parseInt(requestBody.getOrder().get(0).get(DataTablesInput.OrderCriterias.column));
+
+        // 어떤 컬럼을 기준으로 정렬할 것인지 컬럼 이름을 가져옴 컬럼들().get(culumnNum) 이용
+        String culumn = requestBody.getColumns().get(culumnNum).getData();
+
+        // 순차 검색인지 역순 검색인지
+        String order = requestBody.getOrder().get(0).get(DataTablesInput.OrderCriterias.dir);
+
+        // 무엇을 기준으로 검색할 것인지  where id = '뫄뫄' 의 id
+        // String searchWhere = requestBody.getData().get("searchWhere");
+
         int page = start / length;
         Pageable pageable = PageRequest.of(page, length, Sort.Direction.ASC, "id");
         Page<Member> data = memberRepository.findByNameContains(search, pageable);
