5ee0cd9d70216b601e1ab2ccbadd28926731f7e0
yjchoi <yjchoi@unisemiot.com>
2022-04-15 10:15:38 +0900

    2022-04-15 - 검색 방식 변경

--- src/main/java/com/example/new_test/controller/MainRestController.java
+++ src/main/java/com/example/new_test/controller/MainRestController.java
@@ -1,5 +1,6 @@
 package com.example.new_test.controller;
 
+import com.example.new_test.entity.Column;
 import com.example.new_test.entity.DataTablesInput;
 import com.example.new_test.entity.DataTablesOutput;
 import com.example.new_test.entity.MemberDto;
@@ -22,23 +23,30 @@ public class MainRestController {
         int length = requestBody.getLength();
 
         // 검색하고 싶은 검색어
-        String search = requestBody.getSearch().get(DataTablesInput.SearchCriterias.value);
+        String search = "";
+        // 무엇을 기준으로 검색할 것인지  where id = '뫄뫄' 의 id
+        String searchType = "name";
 
-        // 컬럼 이름을 가져오기 위해 몆 번째 컬럼인지 숫자를 가져옴
-        int culumnNum = Integer.parseInt(requestBody.getOrder().get(0).get(DataTablesInput.OrderCriterias.column));
+        List<Column> columns = requestBody.getColumns();
+        for (int i = 0; i < columns.size(); i++) {
+            String columnData = columns.get(i).getSearch().get(DataTablesInput.SearchCriterias.value);
+            if(!columnData.equals("") && columnData != null){
+                search = columnData; // 검색어
+                searchType = columns.get(i).getData(); //검색할 컬럼명
+            }
+        }
+        // 컬럼 이름을 가져오기 위해 몆 번째 컬럼인지 숫자를 가져옴 기본 0
+        int columnNum = Integer.parseInt(requestBody.getOrder().get(0).get(DataTablesInput.OrderCriterias.column));
 
         // 어떤 컬럼을 기준으로 정렬할 것인지 컬럼 이름을 가져옴 컬럼들().get(culumnNum) 이용
-        String culumn = requestBody.getColumns().get(culumnNum).getData();
+        String column = requestBody.getColumns().get(columnNum).getData();
 
         // 순차 검색인지 역순 검색인지
         String order = requestBody.getOrder().get(0).get(DataTablesInput.OrderCriterias.dir);
 
-        // 무엇을 기준으로 검색할 것인지  where id = '뫄뫄' 의 id
-        String searchType = requestBody.getData().get("searchType");
+        // System.out.println("search : "+search+", culumn : "+column+", order : "+order+", searchType : "+ searchType);
 
-        System.out.println("search : "+search+", culumn : "+culumn+", order : "+order+", searchType : "+searchType);
-
-        List<MemberDto> data = memberMybatiseRepository.findData(start, length, search, culumn, order, searchType);
+        List<MemberDto> data = memberMybatiseRepository.findData(start, length, search, column, order, searchType);
         int total = memberMybatiseRepository.findDataTotalCount(search, searchType);
 
         DataTablesOutput output = DataTablesOutput.builder()
