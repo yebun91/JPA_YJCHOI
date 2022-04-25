e9e78b301fc50605452add902f3ee683a5e7dc34
yjchoi <yjchoi@unisemiot.com>
2022-04-25 10:18:42 +0900

    2022-04-25 - 향상된 for문 사용

--- src/main/java/com/example/new_test/controller/MainRestController.java
+++ src/main/java/com/example/new_test/controller/MainRestController.java
@@ -1,13 +1,8 @@
 package com.example.new_test.controller;
 
 import com.example.new_test.entity.*;
-import com.example.new_test.entity.QMember;
 import com.example.new_test.mapper.MemberMapper;
-import com.example.new_test.repository.MemberRepository;
 import com.example.new_test.repository.QMemberRepository;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.PathBuilder;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,22 +29,22 @@ public class MainRestController {
         int length = requestBody.getLength();
 
         HashMap<String, String> searchMap = new HashMap<>();
-        List<Column> columns = requestBody.getColumns();
-        for (int i = 0; i < columns.size(); i++) {
-            String columnData = columns.get(i).getSearch().get(DataTablesInput.SearchCriterias.value);
+        for (Column column : requestBody.getColumns()) {
+            String columnData = column.getSearch().get(DataTablesInput.SearchCriterias.value);
             if(!columnData.equals("") && columnData != null){
-                searchMap.put(columns.get(i).getData(), columnData);
+                searchMap.put(column.getData(), columnData);
             }
         }
 
         ArrayList orderList = new ArrayList();
-        List<Map<DataTablesInput.OrderCriterias, String>> arrayOrder = requestBody.getOrder();
-        for (int i = 0; i < arrayOrder.size(); i++) {
-            String columnNum = arrayOrder.get(i).get(DataTablesInput.OrderCriterias.column);
+
+        for (Map<DataTablesInput.OrderCriterias, String> arrayOrder : requestBody.getOrder()) {
+            String columnNum = arrayOrder.get(DataTablesInput.OrderCriterias.column);
             String column = requestBody.getColumns().get(Integer.parseInt(columnNum)).getData();
-            String dir = arrayOrder.get(i).get(DataTablesInput.OrderCriterias.dir);
+            String dir = arrayOrder.get(DataTablesInput.OrderCriterias.dir);
             orderList.add(column+" "+dir);
         }
+
         String order = String.join(", ", orderList);
 
         List<Member> data = memberMybatiseRepository.findData(start, length, order, searchMap);
@@ -74,10 +68,11 @@ public class MainRestController {
 
         HashMap<String, String> searchMap = new HashMap<>();
         List<Column> columns = requestBody.getColumns();
-        for (int i = 0; i < columns.size(); i++) {
-            String columnData = columns.get(i).getSearch().get(DataTablesInput.SearchCriterias.value);
+
+        for (Column column : columns) {
+            String columnData = column.getSearch().get(DataTablesInput.SearchCriterias.value);
             if(!columnData.equals("") && columnData != null){
-                searchMap.put(columns.get(i).getData(), columnData);
+                searchMap.put(column.getData(), columnData);
             }
         }
 
@@ -100,9 +95,9 @@ public class MainRestController {
             orderList.add(new Sort.Order(direction, orderColumn));
         }
         Sort sort = Sort.by(orderList);
-        Pageable pageable1 = PageRequest.of(page, length, sort);
+        Pageable pageable = PageRequest.of(page, length, sort);
 
-        Page<Member> data = qMemberRepository.searchQueryDSL(searchMap, pageable1, orderMap);
+        Page<Member> data = qMemberRepository.searchQueryDSL(searchMap, pageable, orderMap);
         int total = (int)data.getTotalElements();
         DataTablesOutput output = DataTablesOutput.builder()
                 .draw(draw)
